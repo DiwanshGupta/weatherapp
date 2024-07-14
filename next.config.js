fd551a3bc1d5099b36ddb620dd8895f7921acf7e
DiwanshGupta <dewanshgupta120@gmail.com>
2024-07-15 00:34:59 +0530

    fix dynamic background api bug

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
 /** @type {import('next').NextConfig} */
+const { config } = require('dotenv');
+
+config();
+
 const nextConfig = {
   reactStrictMode: true,
-};
-
-module.exports = {
   images: {
     domains: [
-      "images.unsplash.com",
-      "openweathermap.org",
-      "source.unsplash.com",
+      'images.unsplash.com',
+      'openweathermap.org',
+      'source.unsplash.com',
     ],
   },
+  env: {
+    WEATHER_KEY: process.env.WEATHER_KEY,
+    UNSPLASH_ACCESS_KEY: process.env.UNSPLASH_ACCESS_KEY,
+  },
 };
+
+module.exports = nextConfig;
